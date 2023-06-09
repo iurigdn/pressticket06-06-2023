bea251a0e5ba9b30ff6d1ce4fbaa107582c76cfb
iurigdn <119022772+iurigdn@users.noreply.github.com>
2023-06-09 15:56:13 -0300

    Update index.js

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -123,19 +123,7 @@ const Login = () => {
             className={classes.submit}
           >
             {i18n.t("login.buttons.submit")}
-          </Button>
-          <Grid container>
-            <Grid item>
-              <Link
-                href="#"
-                variant="body2"
-                component={RouterLink}
-                to="/signup"
-              >
-                {i18n.t("login.buttons.register")}
-              </Link>
-            </Grid>
-          </Grid>
+          </Button>          
         </form>
       </div>
       <Box mt={8}><Copyright /></Box>
